8431c08324066fb7ba7b54e55a87ff9258307470
Tyler Turnipseed <tyler@techturnip.us>
2019-05-07 20:28:37 -0400

    Added reference to '.articles' wrapper in the Article class constructor, added closeArticle() method, added '.close' element to HTML, added '.close' ref in constructor, added event listener to closeBtn, added functionality to remove the '.article' element when the closeBtn event listener fires off

--- Article/Article.js
+++ Article/Article.js
@@ -4,19 +4,33 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+
+    this.articleWrapper = this.domElement.parentNode;
+    this.closeBtn = this.domElement.querySelector('.close');
+
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => {
       this.expandArticle();
+    });
+
+    this.closeBtn.addEventListener('click', () => {
+      this.closeArticle();
     });
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+  }
+
+  closeArticle() {
+    this.articleWrapper.removeChild(this.domElement);
   }
 }
 
